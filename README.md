22ea7c230aedadd8f6d5df88ebebaf2381ebec7b
Kurtis Rainbolt-Greene <kurtis@rainbolt-greene.online>
2020-12-01 02:31:17 -0800

    Hello, css-to-css-in-js

---
+++ README.md
@@ -0,0 +1,7 @@
+# cssToCssInJs
+
+``` js
+import cssToCssInJs from "css-to-css-in-js";
+
+cssToCssInJs(".a {color: red}") === "export const aStyle = {color: \"red\"};"
+```
---
+++ babel.config.js
@@ -0,0 +1,37 @@
+module.exports = {
+  presets: [
+    "@babel/preset-typescript",
+  ],
+  plugins: [
+    "@babel/plugin-proposal-object-rest-spread",
+  ],
+  env: {
+    main: {
+      presets: [
+        ["@babel/preset-env", {modules: "cjs", useBuiltIns: "usage", corejs: 3, targets: {node: 14}}],
+        "minify",
+      ],
+    },
+    module: {
+      presets: [
+        ["@babel/preset-env", {modules: false, useBuiltIns: "usage", corejs: 3, targets: {node: 14}}],
+        "minify",
+      ],
+    },
+    browser: {
+      sourceMaps: "inline",
+      presets: [
+        ["@babel/preset-env", {useBuiltIns: "usage", corejs: 3}],
+        "minify",
+      ],
+    },
+    test: {
+      presets: [
+        ["@babel/preset-env", {useBuiltIns: "usage", corejs: 3, targets: {node: "current"}}],
+      ],
+      plugins: [
+        "annotate-console-log",
+      ],
+    },
+  },
+};
